d4307406ed480b56980c4cc88dfff61978512582
RajnikRails <rajnik.rails@gmail.com>
2024-04-25 17:41:37 +0530

    refactor: make add() to work with two numbers make add() method of class StringCalculator to return addition of entered two number seperated by ,(comma)

--- src/string_calculator.rb
+++ src/string_calculator.rb
@@ -2,6 +2,15 @@ class StringCalculator
   def add(numbers)
     entered_numbers = numbers.strip
     return 0 if entered_numbers.empty?
-    return entered_numbers.to_i
+
+    sum = 0
+    if entered_numbers.include?(',')
+      splitted_array = entered_numbers.split(',')
+      sum = splitted_array[0].to_i + splitted_array[1].to_i
+    else
+      sum = entered_numbers.to_i
+    end
+    
+    sum
   end
 end
