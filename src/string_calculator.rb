943c42c0addf9e5c6f7e39a200c4e317c08d24cb
RajnikRails <rajnik.rails@gmail.com>
2024-04-25 18:32:54 +0530

    refactor: make add() method of StringCalculator to throw error if negative number is entered make add() method to throw 'Negatives not allowed : negativenumber' if entered numbers contains negative number

--- src/string_calculator.rb
+++ src/string_calculator.rb
@@ -4,10 +4,11 @@ class StringCalculator
 
     sum = 0
     numbers.split(',').each do |part|
-      if part.match?(/\D/) 
-        sum += part.ord - 'a'.ord + 1 
+      if part.match?(/\D/)
+        raise "Negatives not allowed : #{part}" if part.to_i.negative?
+        sum += part.ord - 'a'.ord + 1
       else
-        sum += part.to_i 
+        sum += part.to_i
       end
     end
     sum
