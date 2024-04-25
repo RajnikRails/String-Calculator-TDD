fb3a77c8c59a96f428f5ec5cbf3925c10885953d
RajnikRails <rajnik.rails@gmail.com>
2024-04-25 18:49:40 +0530

    refactor: make add() method of StringCalculator to return addition of numbers less then 1000

--- src/string_calculator.rb
+++ src/string_calculator.rb
@@ -4,6 +4,7 @@ class StringCalculator
 
     sum = 0
     numbers.split(',').each do |part|
+      next if part.to_i > 1000
       if part.match?(/\D/)
         raise "Negatives not allowed : #{part}" if part.to_i.negative?
         sum += part.ord - 'a'.ord + 1
