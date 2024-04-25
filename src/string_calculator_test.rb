a54363c329922ec59fc88c2b5ef05b874734bb59
RajnikRails <rajnik.rails@gmail.com>
2024-04-25 21:11:16 +0530

    test: check add() returns addition of odd indices if string starts with "0//"

--- src/string_calculator_test.rb
+++ src/string_calculator_test.rb
@@ -97,5 +97,16 @@ describe StringCalculator do
       result_value = @string_calculator.add(input_value)
       assert_equal expected_value, result_value
     end
+
+    it 'should return addition of odd indices if string starts with "0//"' do
+      delimiter = '***'
+      string_numbers = "1#{delimiter}2#{delimiter}3#{delimiter}"
+      input_value = "0//#{delimiter}\n#{string_numbers}"
+      expected_value = string_numbers.split(delimiter).map(&:to_i).each_with_index.reduce(0) do |sum, (curr_str_no, curr_idx)|
+        curr_idx.odd? ? sum + curr_str_no : sum
+      end
+      result_value = @string_calculator.add(input_value)
+      _(result_value).must_equal expected_value
+    end
   end
 end
