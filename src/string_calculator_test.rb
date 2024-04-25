487c4f7f70d59c467b1351adb89a2ec4ab0f686d
RajnikRails <rajnik.rails@gmail.com>
2024-04-25 18:18:15 +0530

    feat: add get_sum_from_string_arr() method in StringCalculator test file
    added get_sum_from_string_arr() to StringCalculator test file to keep DRY for calculating expected_value

--- src/string_calculator_test.rb
+++ src/string_calculator_test.rb
@@ -1,6 +1,16 @@
 require 'minitest/autorun'
 require_relative 'string_calculator'
 
+def get_sum_from_string_arr(arr)
+  arr.reduce(0) do |sum, curr_str_no|
+    if curr_str_no.match?(/\D/)
+      sum + (curr_str_no.ord - 'a'.ord + 1)
+    else
+      sum + curr_str_no.to_i
+    end
+  end
+end
+
 describe StringCalculator do
   describe "#add" do
     before :all do
@@ -23,29 +33,23 @@ describe StringCalculator do
 
     it "should return the addition of two numbers separated by a comma" do
       input_value = "1,2"
-      expected_value = input_value.split(',').map(&:to_i).sum
+      expected_value = get_sum_from_string_arr(input_value.split(','))
       result_value = @string_calculator.add(input_value)
       assert_equal expected_value, result_value
     end
 
     it "should return the addition of entered unlimited numbers separated by a comma" do
       input_value = "1,2,3,4,5"
-      expected_value = input_value.split(',').map(&:to_i).sum
+      expected_value = get_sum_from_string_arr(input_value.split(','))
       result_value = @string_calculator.add(input_value)
       assert_equal expected_value, result_value
     end
 
     it "should return addition of entered numbers along with alphabets(take its position value) separated by ','" do
       input_value = "1,2,b,4,a"
-      expected_value = input_value.split(',').reduce(0) do |sum, curr_str_no|
-        if curr_str_no.match?(/\D/) # Check if the string contains non-digit characters
-          sum + (curr_str_no.ord - 96) # Convert alphabet to its position value
-        else
-          sum + curr_str_no.to_i
-        end
-      end
+      expected_value = get_sum_from_string_arr(input_value.split(','))
       result_value = @string_calculator.add(input_value)
       assert_equal expected_value, result_value
-    end    
+    end
   end
 end
