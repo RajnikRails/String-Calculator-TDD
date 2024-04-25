446c18b8d6a18cee2d5cd48b0d42ed70594c27b1
RajnikRails <rajnik.rails@gmail.com>
2024-04-25 17:52:28 +0530

    refector: add beforeEach() in StringCalculator tests

--- src/string_calculator_test.rb
+++ src/string_calculator_test.rb
@@ -3,24 +3,28 @@ require_relative 'string_calculator'
 
 describe StringCalculator do
   describe "#add" do
+    before :all do
+      @string_calculator = StringCalculator.new
+    end
+
     it "should returns zero if string is empty" do
-      calculator = StringCalculator.new
-      assert_equal 0, calculator.add("")
+      input_value = ""
+      expected_value = input_value.to_i
+      result_value = @string_calculator.add(input_value)
+      assert_equal expected_value, result_value
     end
 
     it "should returns the number itself if only one number is entered as argument" do
-      calculator = StringCalculator.new
       input_value = "2"
       expected_value = input_value.to_i
-      result_value = calculator.add(input_value)
+      result_value = @string_calculator.add(input_value)
       assert_equal expected_value, result_value
     end
 
     it "should return the addition of two numbers separated by a comma" do
-      calculator = StringCalculator.new
       input_value = "1,2"
       expected_value = input_value.split(',').map(&:to_i).sum
-      result_value = calculator.add(input_value)
+      result_value = @string_calculator.add(input_value)
       assert_equal expected_value, result_value
     end
   end
