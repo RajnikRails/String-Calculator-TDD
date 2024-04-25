3eab48b2fcdb9fbe54e36400526c2541293a0a4c
RajnikRails <rajnik.rails@gmail.com>
2024-04-25 19:18:45 +0530

    refactor: make add() method of StringCalculator to work with delimiter "," and "\n"

--- src/string_calculator.rb
+++ src/string_calculator.rb
@@ -3,7 +3,11 @@ class StringCalculator
     return 0 if numbers.empty?
 
     sum = 0
-    numbers.split(',').each do |part|
+    numbers_arr = numbers.split(",").reduce([]) do |arr, curr_str_no|
+      arr + (curr_str_no.include?("\n") ? curr_str_no.split("\n") : [curr_str_no])
+    end
+
+    numbers_arr.each do |part|
       next if part.to_i > 1000
       if part.match?(/\D/)
         raise "Negatives not allowed : #{part}" if part.to_i.negative?
@@ -12,6 +16,7 @@ class StringCalculator
         sum += part.to_i
       end
     end
+
     sum
   end
 end
